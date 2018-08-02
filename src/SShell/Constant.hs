72dfb5b31e70ae2b8e6ca96e56231b64d7b13f01
吉田大輝 <bob181219@yahoo.co.jp>
2018-08-02 10:22:46 +0900

    Describe SShell.Constant module

---
+++ src/SShell/Constant.hs
@@ -0,0 +1,27 @@
+-- Copyright 2018 Daiki Yoshida
+--
+-- This file is part of SShell.
+--
+-- SShell is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- SShell is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with SShell. If not, see <http://www.gnu.org/licenses/>.
+
+module SShell.Constant where
+
+import System.IO.Error		(IOError)
+import Control.Exception	(displayException)
+
+version :: String
+version = "1.0.0"
+
+unknownException :: IOError -> a
+unknownException e = error $ "unknown exception: " ++ (displayException e)
