0a007caaf4f91839df2ee5d87d43389752e64b28
吉田大輝 <bob181219@yahoo.co.jp>
2018-08-09 15:50:18 +0900

    Modify setup.pl

--- setup.pl
+++ setup.pl
@@ -1,8 +1,29 @@
 use strict;
 use warnings;
+use File::Copy;
 
-print "Paths:\n";
-my @paths = <STDIN>;
+print "(1:Take over from other SShell, 2:Setup new SShell)\n";
+while(1) {
+	print ">";
+	chomp(my $select = <STDIN>);
+	if($select == "1") {
+		&take_over;
+		last;
+	} elsif($select == "2") {
+		&setup;
+		last;
+	}
+}
 
-open(PATH, "> ./data/PATH") or die "Error: could not open path-file\n";
-print PATH $_ for @paths;
+sub take_over {
+	print "old SShell's directory: ";
+	chomp(my $directory = <STDIN>);
+	copy("$directory/data/PATH", "./data/PATH");
+}
+
+sub setup {
+	print "Paths:\n";
+	my @paths = <STDIN>;
+	open(PATHS, "> ./data/PATH") or die "Error: failed open path-file\n";
+	print PATHS $_ for @paths;
+}
