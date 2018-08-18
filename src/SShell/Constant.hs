e8c62b816077f88369952059de5ece295a7a67df
吉田大輝 <bob181219@yahoo.co.jp>
2018-08-18 12:43:26 +0900

    Modify version constant in SShell.Constant module

--- src/SShell/Constant.hs
+++ src/SShell/Constant.hs
@@ -22,7 +22,7 @@ import Control.Exception	(displayException)
 import System.IO		(hPutStrLn, stderr)
 
 version :: String
-version = "1.0.4"
+version = "1.0.5"
 
 unexceptedException :: IOError -> a
 unexceptedException e = error $ "an unexcepted exception occured: " ++ (displayException e)
